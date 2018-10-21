2c6df6b0e6ad8082109e9f01a60e172498c25ea1
sebasexy <spider_cbaz_10@hotmail.com>
2018-10-21 16:40:21 -0500

    Read File and Print

---
+++ lexer.py
@@ -0,0 +1,6 @@
+class Lexer(object):
+    def __init__(self, source_code):
+        self.source_code = source_code
+    
+    def tokenize(self):
+        print('test')
---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    # Read the current flow source code
+    content = ""
+    with open('test.lang', 'r') as file:
+        content = file.read()
+
+main()
